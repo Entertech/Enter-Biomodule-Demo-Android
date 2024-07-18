07e23c6deba16ffecba258d016db41374edd8fd2
wangkang <1226426603@qq.com>
2024-07-18 11:30:09 +0800

    优化代码

--- affective_sdk_api/src/main/java/cn/entertech/affective/sdk/api/IFinishAffectiveServiceListener.kt
+++ affective_sdk_api/src/main/java/cn/entertech/affective/sdk/api/IFinishAffectiveServiceListener.kt
@@ -3,10 +3,14 @@ package cn.entertech.affective.sdk.api
 import cn.entertech.affective.sdk.bean.Error
 
 interface IFinishAffectiveServiceListener {
-    fun finishBioFail(error: Error?)
-    fun finishAffectiveFail(error: Error?)
+    fun finishBioFail(error: Error?) {
+
+    }
+
+    fun finishAffectiveFail(error: Error?) {
+
+    }
+
     fun finishError(error: Error?)
     fun finishSuccess()
-
-
 }
